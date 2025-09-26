15d161b09cf375a6ee55a4196ba3729380e4665c
Zharif-1135 <zharif.230170004@mhs.unimal.ac.id>
2025-09-26 18:03:38 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import WaterQualityPlatform from './water-quality-platform'
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">🚀 Water Quality Prediction</h1>
+      <WaterQualityPlatform />
+    </div>
+  )
+}
+
+export default App
